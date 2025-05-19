eae03473348790a77948eb18ae78b5b7b9cfdcc7
13ko13 <fko2547008@stu.o-hara.ac.jp>
2025-05-19 18:29:02 +0900

    up

---
+++ souzinotensai/Assets/sakamotoko/BossController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossController : MonoBehaviour
+{
+    public int maxHP = 5;
+    public int currentHP;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHP = maxHP;
+    }
+
+    // Update is called once per frame
+    void TakeDamage()
+    {
+        currentHP -= 1;
+
+        if(currentHP <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject); 
+    }
+}

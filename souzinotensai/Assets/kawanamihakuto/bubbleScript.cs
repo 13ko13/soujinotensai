12f599b06c3a55ac248eb2d6eb6fb93753011edf
kawanamihakuto <fko2547026@stu.o-hara.ac.jp>
2025-05-23 18:49:04 +0900

    hakuto

--- souzinotensai/Assets/kawanamihakuto/bubbleScript.cs
+++ souzinotensai/Assets/kawanamihakuto/bubbleScript.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class bubbleScript : MonoBehaviour
 {
-    int k;
-    int shot;
+    public float speed = 0.1f;
+   
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +16,6 @@ public class bubbleScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(0,0,0);
+       
     }
 }
--- souzinotensai/Assets/kawanamihakuto/playerScript.cs
+++ souzinotensai/Assets/kawanamihakuto/playerScript.cs
@@ -5,8 +5,18 @@ using UnityEngine;
 public class player : MonoBehaviour
 {
     public GameObject bubblePrefab;
-    public Transform firepoit_w;
-    int x =0;
+    public Transform firepoit;
+    public float speed = 0.1f;
+
+    enum Dir 
+    {
+        Up,
+        Down,
+        Left,
+        Right,
+    }
+
+    Dir dir = Dir.Up;
     // Start is called before the first frame update
     void Start()
     {
@@ -29,36 +39,52 @@ public class player : MonoBehaviour
         {
           transform.eulerAngles=new Vector3(0, 0, 0);
                     this.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 50);
-            x = 1;    
+            dir = Dir.Up;    
         }
         //Sキーで下に動く
         if (Input.GetKeyDown(KeyCode.S))
         {
             transform.eulerAngles = new Vector3(0, 0, 180);
             this.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -50);
-            x = 2;
+            dir = Dir.Down;
         }
         //Aキーで左に動く
         if (Input.GetKeyDown(KeyCode.A))
         {
             transform.eulerAngles = new Vector3(0, 0, 90);
             this.GetComponent<Rigidbody2D>().velocity = new Vector2(-50,0);
-            x = 3;
+            dir = Dir.Left;
         }
         //Dキーで右に動く
         if (Input.GetKeyDown(KeyCode.D))
         {
             transform.eulerAngles = new Vector3(0, 0, -90);
             this.GetComponent<Rigidbody2D>().velocity = new Vector2(50, 0);
-            x = 4;
+            Debug.Log("4");
+            dir = Dir.Right;
         }
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            if (x == 1)
+            if (dir == Dir.Up)
             {
-                Instantiate(bubblePrefab, firepoit_w.position, transform.rotation);
-
+                Instantiate(bubblePrefab, firepoit.position, transform.rotation);
+                //transform.Translate(0, speed, 0);
+            }
+            if (dir == Dir.Down)
+            {
+                Instantiate(bubblePrefab, firepoit.position, transform.rotation);
+                //transform.Translate( 0, -speed, 0);
+            }
+            if (dir == Dir.Left)
+            {
+                Instantiate(bubblePrefab, firepoit.position, transform.rotation);
+                //transform.Translate(-speed, 0,  0);
+            }
+            if (dir == Dir.Right)
+            {
+                Instantiate(bubblePrefab, firepoit.position, transform.rotation);
+                //transform.Translate(speed, 0,  0);
             }
         }
     }

09015eea34014760be39ae158808e2751b56f04d
kawanamihakuto <fko2547026@stu.o-hara.ac.jp>
2025-05-23 14:44:41 +0900

    hakuto

--- souzinotensai/Assets/kawanamihakuto/playerScript.cs
+++ souzinotensai/Assets/kawanamihakuto/playerScript.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class player : MonoBehaviour
 {
-    
+    public GameObject bubblePrefab;
+    public Transform firepoit_w;
+    int x =0;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,31 +27,39 @@ public class player : MonoBehaviour
         //Wキーで上に動く
         if (Input.GetKeyDown(KeyCode.W))
         {
-          
+          transform.eulerAngles=new Vector3(0, 0, 0);
                     this.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 50);
-              
+            x = 1;    
         }
         //Sキーで下に動く
         if (Input.GetKeyDown(KeyCode.S))
         {
-           
-                    this.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -50);
-             
+            transform.eulerAngles = new Vector3(0, 0, 180);
+            this.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -50);
+            x = 2;
         }
         //Aキーで左に動く
         if (Input.GetKeyDown(KeyCode.A))
         {
-
+            transform.eulerAngles = new Vector3(0, 0, 90);
             this.GetComponent<Rigidbody2D>().velocity = new Vector2(-50,0);
-
+            x = 3;
         }
         //Dキーで右に動く
         if (Input.GetKeyDown(KeyCode.D))
         {
-
+            transform.eulerAngles = new Vector3(0, 0, -90);
             this.GetComponent<Rigidbody2D>().velocity = new Vector2(50, 0);
-
+            x = 4;
         }
-        
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (x == 1)
+            {
+                Instantiate(bubblePrefab, firepoit_w.position, transform.rotation);
+
+            }
+        }
     }
 }

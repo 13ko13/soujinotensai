7e0c07adcf30523f90281193731d3e405bce8db6
ko1307 <fko2547008@stu.o-hara.ac.jp>
2025-05-26 13:25:41 +0900

    up

--- souzinotensai/Assets/kawanamihakuto/playerScript.cs
+++ souzinotensai/Assets/kawanamihakuto/playerScript.cs
@@ -9,88 +9,88 @@ public class player : MonoBehaviour
     public float speed = 0.1f;
     private float timeBetweenShot = 3.0f;//ãÖÇçƒìxë≈ÇƒÇÈÇÊÇ§Ç…Ç»ÇÈÇ‹Ç≈ÇÃéûä‘
     private float timer;
-    enum Dir 
-    {
-        Up,
-        Down,
-        Left,
-        Right,
-    }
+//    enum Dir 
+//    {
+//        Up,
+//        Down,
+//        Left,
+//        Right,
+//    }
 
-    Dir dir = Dir.Up;
-    // Start is called before the first frame update
-    void Start()
-    {
-        Application.targetFrameRate = 60;
-    }
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if(collision.gameObject.tag=="wall")
-        {
-            Debug.Log("aaaaaa");
+//    Dir dir = Dir.Up;
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+//        Application.targetFrameRate = 60;
+//    }
+//    private void OnTriggerEnter2D(Collider2D collision)
+//    {
+//        if(collision.gameObject.tag=="wall")
+//        {
+//            Debug.Log("aaaaaa");
             
-        }
-    }
-    // Update is called once per frame
-    void Update()
-    {
-        this.GetComponent<Rigidbody2D>().linearVelocity = new Vector2(0, 0);//ÔøΩÔøΩÔøΩÔøΩÔøΩÔøΩ
-        //WÔøΩLÔøΩ[ÔøΩ≈èÔøΩ…ìÔøΩÔøΩÔøΩ
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-          transform.eulerAngles=new Vector3(0, 0, 0);
-                    this.GetComponent<Rigidbody2D>().linearVelocity = new Vector2(0, 50);
-            dir = Dir.Up;    
-        }
-        //SÔøΩLÔøΩ[ÔøΩ≈âÔøΩÔøΩ…ìÔøΩÔøΩÔøΩ
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            transform.eulerAngles = new Vector3(0, 0, 180);
-            this.GetComponent<Rigidbody2D>().linearVelocity = new Vector2(0, -50);
-            dir = Dir.Down;
-        }
-        //AÔøΩLÔøΩ[ÔøΩ≈çÔøΩÔøΩ…ìÔøΩÔøΩÔøΩ
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            transform.eulerAngles = new Vector3(0, 0, 90);
-            this.GetComponent<Rigidbody2D>().linearVelocity = new Vector2(-50,0);
-            dir = Dir.Left;
-        }
-        //DÔøΩLÔøΩ[ÔøΩ≈âEÔøΩ…ìÔøΩÔøΩÔøΩ
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            transform.eulerAngles = new Vector3(0, 0, -90);
-            this.GetComponent<Rigidbody2D>().linearVelocity = new Vector2(50, 0);
-            Debug.Log("4");
-            dir = Dir.Right;
-        }
+//        }
+//    }
+//    // Update is called once per frame
+//    void Update()
+//    {
+//        this.GetComponent<Rigidbody2D>().linearVelocity = new Vector2(0, 0);//ÅEΩÅEΩÅEΩÅEΩÅEΩÅEΩ
+//        //WÅEΩLÅEΩ[ÅEΩ≈èÔøΩ…ìÔøΩÅEΩÅEΩ
+//        if (Input.GetKeyDown(KeyCode.W))
+//        {
+//          transform.eulerAngles=new Vector3(0, 0, 0);
+//                    this.GetComponent<Rigidbody2D>().linearVelocity = new Vector2(0, 50);
+//            dir = Dir.Up;    
+//        }
+//        //SÅEΩLÅEΩ[ÅEΩ≈âÔøΩÅEΩ…ìÔøΩÅEΩÅEΩ
+//        if (Input.GetKeyDown(KeyCode.S))
+//        {
+//            transform.eulerAngles = new Vector3(0, 0, 180);
+//            this.GetComponent<Rigidbody2D>().linearVelocity = new Vector2(0, -50);
+//            dir = Dir.Down;
+//        }
+//        //AÅEΩLÅEΩ[ÅEΩ≈çÔøΩÅEΩ…ìÔøΩÅEΩÅEΩ
+//        if (Input.GetKeyDown(KeyCode.A))
+//        {
+//            transform.eulerAngles = new Vector3(0, 0, 90);
+//            this.GetComponent<Rigidbody2D>().linearVelocity = new Vector2(-50,0);
+//            dir = Dir.Left;
+//        }
+//        //DÅEΩLÅEΩ[ÅEΩ≈âEÅEΩ…ìÔøΩÅEΩÅEΩ
+//        if (Input.GetKeyDown(KeyCode.D))
+//        {
+//            transform.eulerAngles = new Vector3(0, 0, -90);
+//            this.GetComponent<Rigidbody2D>().linearVelocity = new Vector2(50, 0);
+//            Debug.Log("4");
+//            dir = Dir.Right;
+//        }
 
-        timer += Time.deltaTime;//É^ÉCÉ}Å[ÇÃéûä‘ÇìÆÇ©Ç∑
+//        timer += Time.deltaTime;//É^ÉCÉ}Å[ÇÃéûä‘ÇìÆÇ©Ç∑
 
-        if (Input.GetKeyDown(KeyCode.Space)&&timer>timeBetweenShot)
-        {
-            timer = 0.0f;//É^ÉCÉ}Å[ÇÃéûä‘Ç0Ç…ñﬂÇ∑
+//        if (Input.GetKeyDown(KeyCode.Space)&&timer>timeBetweenShot)
+//        {
+//            timer = 0.0f;//É^ÉCÉ}Å[ÇÃéûä‘Ç0Ç…ñﬂÇ∑
 
-            if (dir == Dir.Up)
-            {
-                Instantiate(bubblePrefab, firepoit.position, transform.rotation);
-                //transform.Translate(0, speed, 0);
-            }
-            if (dir == Dir.Down)
-            {
-                Instantiate(bubblePrefab, firepoit.position, transform.rotation);
-                //transform.Translate( 0, -speed, 0);
-            }
-            if (dir == Dir.Left)
-            {
-                Instantiate(bubblePrefab, firepoit.position, transform.rotation);
-                //transform.Translate(-speed, 0,  0);
-            }
-            if (dir == Dir.Right)
-            {
-                Instantiate(bubblePrefab, firepoit.position, transform.rotation);
-                //transform.Translate(speed, 0,  0);
-            }
-        }
-    }
+//            if (dir == Dir.Up)
+//            {
+//                Instantiate(bubblePrefab, firepoit.position, transform.rotation);
+//                //transform.Translate(0, speed, 0);
+//            }
+//            if (dir == Dir.Down)
+//            {
+//                Instantiate(bubblePrefab, firepoit.position, transform.rotation);
+//                //transform.Translate( 0, -speed, 0);
+//            }
+//            if (dir == Dir.Left)
+//            {
+//                Instantiate(bubblePrefab, firepoit.position, transform.rotation);
+//                //transform.Translate(-speed, 0,  0);
+//            }
+//            if (dir == Dir.Right)
+//            {
+//                Instantiate(bubblePrefab, firepoit.position, transform.rotation);
+//                //transform.Translate(speed, 0,  0);
+//            }
+//        }
+//    }
 }

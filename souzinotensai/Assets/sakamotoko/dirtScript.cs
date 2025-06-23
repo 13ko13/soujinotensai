6d507956956b133cf6570b19b46f88fa00ddadbd
ko1307 <fko2547008@stu.o-hara.ac.jp>
2025-06-23 18:55:37 +0900

    up

--- souzinotensai/Assets/sakamotoko/dirtScript.cs
+++ souzinotensai/Assets/sakamotoko/dirtScript.cs
@@ -2,10 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class sakamotodirtScript : MonoBehaviour
+public class sakamotokou_dirtScript : MonoBehaviour
 {
+    
     public float timer;
     public float timeBetweenShot = 0.1f;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,29 +19,40 @@ public class sakamotodirtScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         timer += Time.deltaTime;
+
     }
+
+    
+   
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("player")) // playerかどうかのタグをチェック
         {
             //Debug.Log("player");
-
+           
             Destroy(gameObject);//汚れを消す
         }
-        if (collision.gameObject.CompareTag("sikaku"))
+        if(collision.gameObject.CompareTag("sikaku"))
         {
             Destroy(gameObject);//汚れを消す
         }
+
+      //if(collision.gameObject.CompareTag("dirt")) //汚れかどうかのタグをチェック
+      //{
+      //OldDirtDelete.SpawnDirt(new Vector3 );
+      //}
 
         if (collision.gameObject.CompareTag("dirt"))//汚れかどうかのタグをチェック
         {
-            if (timer > timeBetweenShot)
+            if (timer > timeBetweenShot) 
             {
                 Destroy(gameObject);
                 Debug.Log("なくなった");
             }
-
+            
         }
     }
 }

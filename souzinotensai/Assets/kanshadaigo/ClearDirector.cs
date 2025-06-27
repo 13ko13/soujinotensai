fa0b4fea867fa8ab5f5758672c123fb2db2fe0d8
13ko13 <208754810+13ko13@users.noreply.github.com>
2025-06-27 15:51:55 +0900

    up

--- souzinotensai/Assets/kanshadaigo/ClearDirector.cs
+++ souzinotensai/Assets/kanshadaigo/ClearDirector.cs
@@ -15,10 +15,24 @@ public class ClearDirector : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Alpha2))
         {
-            SceneManager.LoadScene("kudousyouta");
+            SceneManager.LoadScene("Stage2");
         }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            SceneManager.LoadScene("Stage3");
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            SceneManager.LoadScene("Stage4");
+        }
+
+
+
+
+
+
         if (Input.GetKeyDown(KeyCode.Return))
         {
             SceneManager.LoadScene("MenuScene");

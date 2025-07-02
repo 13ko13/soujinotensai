51190fced4dbc38aba0abc36871cefea240b7b5d
ko1307 <fko2547008@stu.o-hara.ac.jp>
2025-07-02 18:56:02 +0900

    up

--- souzinotensai/Assets/sakamotoko/BossController.cs
+++ souzinotensai/Assets/sakamotoko/BossController.cs
@@ -1,11 +1,15 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BossController : MonoBehaviour
 {
-    public int maxHP = 5;
+    [SerializeField] private Image hpBarImage;
+    [SerializeField] private Sprite[] hpBarSprites; //スプライト4枚
+    [SerializeField] private int maxHP = 5;
     public int currentHP;
     public int dirt;
     public GameObject Dirt;
@@ -20,6 +24,8 @@ public class BossController : MonoBehaviour
     void TakeDamage()
     {
         currentHP -= 1;
+
+        UpdateHPBar();//HPバーの更新を行う
 
         if (currentHP <= 0)
         {
@@ -41,6 +47,26 @@ public class BossController : MonoBehaviour
     }
     // Update is called once per frame
    
+    //HPバーの更新
+    private void UpdateHPBar()
+    {
+        if(currentHP == 5.0f)
+        {
+            hpBarImage.sprite = hpBarSprites[0]; //緑
+        }
+        else if(currentHP >= 3.0f)
+        {
+            hpBarImage.sprite = hpBarSprites[1]; //黄
+        }
+        else if (currentHP >= 1.0f)
+        {
+            hpBarImage.sprite = hpBarSprites[2]; //ピンク
+        }
+        else
+        {
+            hpBarImage.sprite = hpBarSprites[3]; //グレー
+        }
+    }
 
     void Die()
     {
